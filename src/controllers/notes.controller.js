d9a8f1a599aa9094d653ce1e3ed62807b081da33
Nathan Robertson <nathanrobertson1997@gmail.com>
2025-04-08 05:25:10 +1000

    Added 'updateNote' & 'deleteNote' to the notesConrtoller, Added new Notes Routes,

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,20 +1,48 @@
 import { DB } from '../database.js';
 
 export async function getNotes() {
-    const [rows] = await DB.query('SELECT * FROM notes');
-    return rows;
+    console.log("Retrieving notes...");
+    const [notes] = await DB.query('SELECT * FROM notes');
+    console.log("Retrieved notes", notes);
+    return notes;
 }
 
 export async function getNote(id) {
+    console.log("Retrieving note...");
     const [note] = await DB.query(`
         SELECT * FROM notes
         WHERE id = ?`, [id]);
+    console.log("Retrieved note", note[0]);
     return note[0];
 }
 
 export async function createNote(title, contents) {
+    console.log("Create note...");
     const [result] = await DB.query(`
         INSERT INTO notes (title, contents)
         VALUES (?, ?)`, [title, contents]);
-    return getNote(result.insertId);
+    const note =  await getNote(result.insertId)
+    console.log("Updated note", note);
+    return note;
+}
+
+export async function updateNote(id, title, contents) {
+    console.log("Updating note...");
+    const [result] = await DB.query(`
+        UPDATE notes set
+        title = ?,
+        contents = ?
+        WHERE id = ?`, [title, contents, id]);
+    const note =  await getNote(id)
+    console.log("Updated note", note);
+    return note;
+}
+
+export async function deleteNote(id) {
+    console.log("Deleting note...");
+    const [note] = await DB.query(`
+        DELETE FROM notes
+        WHERE id = ?`, [id]);
+    console.log("Deleted note");
+    return;
 }
--- src/routes/notes.route.js
+++ src/routes/notes.route.js
@@ -1,28 +1,47 @@
 import express from 'express';
 
 // Controllers.
-import { getNotes, getNote, createNote } from '../controllers/notes.controller.js';
+import * as notesController from '../controllers/notes.controller.js';
 
 // Constants.
 import { HTTP_RESPONSE } from '../constants/common.constants.js';
 
+// Initialise Express router.
 const notesRouter = express.Router();
 
+// Retrieve all notes.
 notesRouter.get('/', async (req, res) => {
-    const notes = await getNotes();
+    const notes = await notesController.getNotes();
     res.send(notes);
 });
 
+// Retrieve specific note by ID.
 notesRouter.get('/:id', async (req, res) => {
     const { id } = req.params;
-    const note = await getNote(id);
+    const note = await notesController.getNote(id);
     res.send(note);
 });
 
+// Create a new note.
 notesRouter.post('/', async (req, res) => {
     const { title, contents } = req.body;
-    const note = await createNote(title, contents);
+    const note = await notesController.createNote(title, contents);
     res.status(HTTP_RESPONSE.CREATED).send(note);
+});
+
+// Update an existing note.
+notesRouter.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, contents } = req.body;
+    const note = await notesController.updateNote(id, title, contents);
+    res.status(HTTP_RESPONSE.OK).send(note);
+});
+
+// Create a new note.
+notesRouter.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const note = await notesController.deleteNote(id);
+    res.status(HTTP_RESPONSE.OK).send(note);
 });
 
 export default notesRouter;

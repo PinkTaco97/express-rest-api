0c37463bee6b648080ade2e0eadf853573bff88e
Nathan Robertson <nathanrobertson1997@gmail.com>
2025-05-02 15:46:38 +1000

    Refactor notes controller: standardize import statements and improve code formatting for consistency

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
 // Routes.
-import NotesRouter from './routes/notes.route.js';
-import UserRouter from './routes/user.route.js';
+import NotesRouter from "./routes/notes.route.js";
+import UserRouter from "./routes/users.route.js";
 
 // Constants.
-import { HTTP_RESPONSE } from './constants/common.constants.js';
+import { HTTP_RESPONSE } from "./constants/common.constants.js";
 
 // Environment variables.
 dotenv.config();
@@ -19,10 +19,10 @@ app.use(express.json());
 
 // CROSS-ORIGIN-ALLOW-ALL
 const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 app.use(cors(corsOptions));
 
@@ -31,10 +31,10 @@ app.use(`${SERVER_BASE_PATH}/notes`, NotesRouter);
 app.use(`${SERVER_BASE_PATH}/users`, UserRouter);
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(HTTP_RESPONSE.INTERNAL_SERVER_ERROR).send('Something broke!');
+  console.error(err.stack);
+  res.status(HTTP_RESPONSE.INTERNAL_SERVER_ERROR).send("Something broke!");
 });
 
 app.listen(SERVER_PORT, () => {
-    console.log(`Server is running on port ${SERVER_PORT}...`);
+  console.log(`Server is running on port ${SERVER_PORT}...`);
 });
